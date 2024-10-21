9e0d8f80993d3d7fd2de8831b9901854cfc9d3c5
Jiachen (Chris) Lu <134972140+jlu1211@users.noreply.github.com>
2024-10-21 13:48:47 -0700

    adding updated dirichlet boundary code

--- CODE/DirichletBD.m
+++ CODE/DirichletBD.m
@@ -1,20 +1,27 @@
-function DB_tf = DirichletBD(bd_x,bd_y,s)
-% Dirichlet Boundary condition is defined here.
-% Gives back 1 if (x,y) is on the Dirichlet boundary condition
-%            0 if not.
-% x and y can be a vector, if so, tf is a vector of 1 and 0s as described
-%above
-DB_int = linspace(0,0.25*pi,length(bd_x));
-DB_points = s.Zorigional(DB_int);
-DB_tf = zeros(size(bd_x));
+function DB_tf = DirichletBD(bd_x, bd_y, s)
+    % DirichletBD: Determines if boundary points are on the Dirichlet boundary
+    % based on the parameterization 't' of the boundary curve.
+    %
+    % bd_x, bd_y: Boundary point x and y coordinates
+    % s: Structure containing the original boundary parameterization
+    % Returns:
+    % DB_tf: 1 for Dirichlet boundary points (0 ≤ t ≤ π), 0 for Neumann points (π < t ≤ 2π)
 
-tol = 1e-6;
-
-for i = 1:length(bd_x)
-    difference = abs(DB_points - (bd_x(i) + 1i*bd_y(i)));
-    if any(difference < tol)
-        DB_tf(i) = 1;
+    DB_tf = zeros(size(bd_x));
+    tol = 1e-6;
+    
+    for i = 1:length(bd_x)
+        % Find the corresponding 't' value for the boundary point (bd_x(i), bd_y(i))
+        point = bd_x(i) + 1i * bd_y(i);  % Complex representation of the boundary point
+        difference = abs(s.original(s.t) - point);  % Difference with boundary points
+        
+        % Find the closest parameter value in 't' that matches the point
+        [~, idx] = min(difference);
+        t_value = s.t(idx);  % The 't' value closest to the boundary point
+        
+        % Check if the 't' value is in the Dirichlet range [0, pi]
+        if t_value >= 0 && t_value <= pi
+            DB_tf(i) = 1;  % This point is on the Dirichlet boundary
+        end
     end
-end
-
 end
