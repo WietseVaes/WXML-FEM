453c4e920911e2d5e39fa368a196ae09e0376d70
kaillahs <kaillahs@uw.edu>
2024-10-23 17:27:28 -0700

    M,S matrices

--- CODE/Build.m
+++ CODE/Build.m
@@ -1,8 +1,9 @@
-function [M,P,S,b] = Build(x,y,elmat,f,elmatbd,g)
+function [M,P,S,b] = Build(x,y,elmat,elmatbd)
 
-n = size(x);
+%took out f and g from input...
+n = length(x);
 
-M 		= sparse(n,n); %Mass matrix
+M       = sparse(n,n); %Mass matrix
 
 P       = sparse(n,n); %Velocity matrix
 
@@ -12,8 +13,20 @@ b 		= zeros(n,1); %Right hand side
 
 
 for i1 = 1:length(elmat(:,1)) % for all internal elements
-    GenerateElementMatrix; %Generate small matrices (Melem, Pelem, Selem)
+    [Melem, Pelem, Selem] = GenerateElementMatrix(elmat(i1,:),x,y); %Generate small matrices (Melem, Pelem, Selem)
+    elmat(i1,:)
 
+    
+    for i = 1:length(elmat(i1,:))
+        for j = 1:length(elmat(i1,:))
+            M(elmat(i1,i),elmat(i1,j)) = M(elmat(i1,i),elmat(i1,j)) + Melem(i,j);
+            S(elmat(i1,i),elmat(i1,j)) = S(elmat(i1,i),elmat(i1,j)) + Selem(i,j);
+        end
+    end
+    %M(i11,i11) = M(i11,i11) + MelemDiag;
+    %M(i12,i12) = M(i12,i12) + MelemDiag;
+    %M(i13,i13) = M(i13,i13) + MelemDiag;
+  
     %Add small matrices to big matrices M,P and S
 
     GenerateElementVector; %calculating b vector over current element (belem)
--- CODE/GenerateElementMatrix.m
+++ CODE/GenerateElementMatrix.m
@@ -1,3 +1,62 @@
 % Generate the matrices of M, P and S over a considered element i1 through
 % theory and numerical methods. you need to output Melem, Pelem, Selem
 % which are three by three matrices in this case
+
+function [Melem, Pelem, Selem] = GenerateElementMatrix(indices,x_total,y_total)
+
+%Melem
+Md = 3;
+elemArea = get_area(indices,x_total,y_total);
+Melem = ((2*elemArea)/factorial(Md+1))*(1 + eye(3));
+
+%Selem
+
+%Solve for alpha, beta, gamma
+
+xc = x_total(indices);
+yc = y_total(indices);
+
+%phi1
+A = zeros(length(xc));
+b1 = [1,0,0]';
+
+for i=1:length(xc)
+    A(i,:) = [1,xc(i),yc(i)];
+end
+
+x1 = A\b1; %solve Ax = b
+
+%phi2
+
+b2 = [0,1,0]';
+x2 = A\b2;
+
+%phi3
+
+b3 = [0,0,1]';
+x3 = A\b3;
+
+X = [x1,x2,x3];
+%x arrays store alpha, beta, then gamma
+
+Selem = zeros(length(xc));
+
+for i=1:length(xc)
+    for j=1:length(yc)
+        Selem(i,j) = (elemArea / 3)*((X(2,j) * X(2,i) )+(X(3,j) * X(3,i) ));
+    end
+end
+
+%Pelem
+
+
+function area = get_area(indices, x_total, y_total)
+xc = x_total(indices);
+yc = y_total(indices);
+area  = 0.5*abs(det([ones(3,1), xc, yc]));
+end
+
+Pelem = 0;
+Selem = 0;
+end
+
