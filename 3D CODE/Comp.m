b8820163490b2ba0ff5eb07ca1dd8a72cc000b4e
Wietse Vaes <wietsev@uw.edu>
2024-11-20 23:03:06 -0800

    Update Comp.m

--- 3D CODE/Comp.m
+++ 3D CODE/Comp.m
@@ -23,7 +23,7 @@ for i70 = 2:nt
     % Update solution for interior points at the current time step
     uu(keep, i70) = (M(keep,keep) - Dt * (P(keep,keep) - D * S(keep,keep))) \ (Dt * b(keep) + M(keep,keep) * uu(keep, i70 - 1) + Mtilde(keep,:) * hn - ((Mtilde(keep,:) - Dt * (Ptilde(keep,:) - D * Stilde(keep,:))) * hnp1));
 
-    disp([num2str(nt/i70,2),'% done'])
+    disp([num2str(i70/nt*100,2),'% done'])
 end
 
 u = zeros(length(x), nt);
