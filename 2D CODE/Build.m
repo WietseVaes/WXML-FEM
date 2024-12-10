31ba104300843e2866a3e50079c604e56bfca195
kaillahs <kaillahs@uw.edu>
2024-12-09 22:08:50 -0800

    minor changes

--- 2D CODE/Build.m
+++ 2D CODE/Build.m
@@ -1,36 +1,26 @@
 function [P, b, Ptilde] = Build(x,y,elmat,elmatbd,f,g,vx,vy, Id)
-
-%took out f and g from input...
 n = length(x);
 
 P       = sparse(n,n); %Velocity matrix
-
-
 b 		= zeros(n,1); %Right hand side
 
 
 for i1 = 1:length(elmat(:,1)) % for all internal elements
-    [Melem, Pelem, Selem] = GenerateElementMatrix(elmat(i1,:),x,y,vx,vy); %Generate small matrices (Melem, Pelem, Selem)
+    [Pelem] = GenerateElementMatrix(elmat(i1,:),x,y,vx,vy);
 
     P(elmat(i1,:), elmat(i1,:)) = P(elmat(i1,:), elmat(i1,:)) + Pelem;
 
-    belem = GenerateElementVector(elmat(i1,:),x,y,f); %calculating b vector over current element (belem)
-
-
+    belem = GenerateElementVector(elmat(i1,:),x,y,f);
     b(elmat(i1,:)) = b(elmat(i1,:)) + belem;
-
-    % add belem to big b
-
 
 end
 
 
 for i1 = 1:length(elmatbd(:,1)) % for all boundary elements
     belem = GenerateBoundaryElementVector(elmatbd(i1,:),x,y,g); %calculating b vector over current neumann boundary element (belem)
-
     b(elmatbd(i1,:)) = b(elmatbd(i1,:)) + belem;
 
-    % add belem to big b
+  
 
 end
 
@@ -40,7 +30,3 @@ P(Id,:) = [];
 P(:,Id) = [];
 
 b(Id) = [];
-
-
-%hvec = h(x(Id),y(Id));
-%size(hvec)
